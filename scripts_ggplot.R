8a864e68651d0037b4c273ce20e76f0c9510d496
Gaspard Martet <gaspard.martet@icm-institute.org>
2025-12-15 10:36:54 +0100

    add correlation circle

--- scripts_ggplot.R
+++ scripts_ggplot.R
@@ -25,3 +25,45 @@ data %>%
        x = "",
        y = "")+
   scale_fill_manual(values = c("#5F9EA0", "#E1B378"))
+
+#-------------------------------------------------------------------------------
+# cercle des corrélations
+#-------------------------------------------------------------------------------
+
+circleFun <- function(center = c(-1,1), diameter = 1, npoints = 100) {
+  r = diameter/2
+  tt <- seq(0, 2*pi,length.out = npoints)
+  xx <- center[1] + r*cos(tt)
+  yy <- center[2] + r*sin(tt)
+  return(data.frame(x = xx, y = yy))
+}
+
+circle <- rbind(circleFun(c(0,0), 2, npoints = 100), circleFun(c(0,0), 1, npoints = 100))
+
+sp.cor <- data.frame(t(round(cor(df[,c("comp1","comp2")], X, use = "pairwise.complete.obs", method = "pearson"), 3)))
+sp.cor$label <- rownames(sp.cor)
+
+ggplot(sp.cor, aes(x = comp1, y = comp2)) +
+  geom_path(aes(x, y), data = circle, col = "gray60", size = .2) + 
+  theme_minimal() +
+  geom_vline(xintercept = 0, col = "gray60", size = .3) + 
+  geom_hline(yintercept = 0, col = "gray60", size = .3) +
+  ggtitle("correlation circle") +
+  geom_point(size = 1.6, shape = 21, color = "white", stroke = .3) +
+  geom_text_repel(aes(label = label), vjust = 0.4, hjust = 0.4, nudge_y = 0.02,
+                  size = 2.3, segment.size  = 0.2,
+                  segment.color = "grey50", show.legend = FALSE) +
+  xlab("Comp 1 : xxx % explained veriance") + 
+  ylab("Comp 2 : xxx % explained variance") + 
+  theme(axis.text = element_text(color = "black", size = 12), 
+        axis.title = element_text(size = 12),
+        legend.title = element_blank(), legend.text = element_text(size = 9),
+        legend.spacing.x = unit(.1, 'cm'),
+        legend.position = "bottom", legend.margin = margin(t = -8),
+        plot.title = element_text(hjust = 0.5, size = 12),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_line(size = .3, color = "gray92")
+  ) +
+  scale_color_manual(values=c("darkred", "dodgerblue4", "springgreen4")) +
+  scale_fill_manual(values=c("darkred", "dodgerblue4", "springgreen4")) +
+  guides(color = guide_legend(override.aes = list(size = 3)))
